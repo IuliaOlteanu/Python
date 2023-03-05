a3b772f08e5f934822ab960d802f7918020db5fe
IuliaOlteanu <73997287+IuliaOlteanu@users.noreply.github.com>
2023-03-05 18:29:59 +0200

    Create afisare.py
    Afisare triunghiulara
    1
    232
    34543
    ....

---
+++ afisare.py
@@ -0,0 +1,16 @@
+def afisare(n):
+    for j in range(1, n + 1):
+        num = j
+        for i in range(1, j + 1):
+            print(num, end="")
+            num = num + 1
+
+        num = num - 2
+        for i in range(1, j):
+            print(num, end="")
+            num = num - 1
+        print()
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    afisare(n)
